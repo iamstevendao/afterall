932d6644abf6d17ec9ca6711a5e570d966882969
fukie <kiendp@kaopiz.com>
2016-05-20 00:16:25 +0700

    done connecting to calendar

--- app/src/main/java/fukie/afterall/MainActivity.java
+++ app/src/main/java/fukie/afterall/MainActivity.java
@@ -1,13 +1,65 @@
 package fukie.afterall;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String[] EVENT_PROJECTION = new String[]{
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 10;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.txtHello);
+        Cursor cur;
+        ContentResolver cr = getContentResolver();
+        Uri uri = CalendarContract.Calendars.CONTENT_URI;
+        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[]{"iamstevendao@gmail.com", "com.google",
+                "iamstevendao@gmail.com"};
+// Submit the query and get a Cursor object back.
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
+            cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+            while (cur.moveToNext()) {
+
+                long calID = cur.getLong(PROJECTION_ID_INDEX);
+                String displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+                String accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+                String ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+
+                // Do something with the values...
+                String s = calID + " " + displayName + " " + accountName + " " + ownerName;
+                textView.setText(s);
+            }
+        }
+
     }
+
+
 }
