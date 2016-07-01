408f36ecff66a1e85e40ad01bbac19cbbf2e00a8
fukie <kiendp@kaopiz.com>
2016-07-01 12:01:25 +0700

    add joda time

--- app/src/main/java/fukie/afterall/utils/Events.java
+++ app/src/main/java/fukie/afterall/utils/Events.java
@@ -1,5 +1,10 @@
 package fukie.afterall.utils;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,6 +19,7 @@ public class Events {
     private int kind;
     private int color;
     private String date;
+    private String dateOfLoop;
     private int diff;
     private int loop;
     private int img;
@@ -45,6 +51,7 @@ public class Events {
             while (today.after(c.getTime())) {
                 c.add(Calendar.YEAR, 1);
             }
+            this.dateOfLoop = sdf.format(c.getTime());
             long diff = c.getTime().getTime() - today.getTime();
             diffDays = (int) (diff / (60 * 60 * 1000 * 24));
         } else {
@@ -81,28 +88,43 @@ public class Events {
     }
 
     public int getId() {return id;}
+//    public String getDiffString() throws Exception {
+//        String string = "";
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+//        Calendar now = Calendar.getInstance();
+//        Calendar event = Calendar.getInstance();
+//        now.setTime(new Date());
+//        event.setTime(sdf.parse(date));
+//        int diffYear = now.get(Calendar.YEAR) - event.get(Calendar.YEAR);
+//        if (event.get(Calendar.MONTH) > now.get(Calendar.MONTH) ||
+//                (event.get(Calendar.MONTH) == now.get(Calendar.MONTH)
+//                        && event.get(Calendar.DATE) > now.get(Calendar.DATE))) {
+//            diffYear--;
+//        }
+//        string += String.valueOf(diffYear) + "years ";
+//        int diffMonth = now.get(Calendar.MONTH) - event.get(Calendar.MONTH);
+//        if (event.get(Calendar.MONTH) > now.get(Calendar.MONTH) ||
+//                (event.get(Calendar.DATE) == now.get(Calendar.DATE))) {
+//            diffMonth--;
+//        }
+//        string += String.valueOf(diffMonth) + "months ";
+//        int diffDay = now.get(Calendar.DATE) - event.get(Calendar.DATE);
+//        string += String.valueOf(diffDay) + "days ";
+//        return string;
+//    }
+
     public String getDiffString() throws Exception {
         String string = "";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        Calendar now = Calendar.getInstance();
-        Calendar event = Calendar.getInstance();
-        now.setTime(new Date());
-        event.setTime(sdf.parse(date));
-        int diffYear = now.get(Calendar.YEAR) - event.get(Calendar.YEAR);
-        if (event.get(Calendar.MONTH) > now.get(Calendar.MONTH) ||
-                (event.get(Calendar.MONTH) == now.get(Calendar.MONTH)
-                        && event.get(Calendar.DATE) > now.get(Calendar.DATE))) {
-            diffYear--;
-        }
-        string += String.valueOf(diffYear) + "years ";
-        int diffMonth = now.get(Calendar.MONTH) - event.get(Calendar.MONTH);
-        if (event.get(Calendar.MONTH) > now.get(Calendar.MONTH) ||
-                (event.get(Calendar.DATE) == now.get(Calendar.DATE))) {
-            diffMonth--;
-        }
-        string += String.valueOf(diffMonth) + "months ";
-        int diffDay = now.get(Calendar.DATE) - event.get(Calendar.DATE);
-        string += String.valueOf(diffDay) + "days ";
+        Date event;
+        if(loop == 0)
+            event = sdf.parse(date);
+        else
+            event = sdf.parse(dateOfLoop);
+        DateTime then = new DateTime(event);
+        DateTime now = new DateTime();
+        Period period = new Period(now, then);
+        string+= period.getYears() + " " + period.getMonths() + " " + (period.getWeeks()*7 + period.getDays());
         return string;
     }
 }
